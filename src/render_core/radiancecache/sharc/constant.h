0756d6cf027941ccdab9dff22125d84f06cfb5b8
Zero <249002031@qq.com>
2025-07-21 00:02:06 +0800

    update sharc

--- src/render_core/radiancecache/sharc/constant.h
+++ src/render_core/radiancecache/sharc/constant.h
@@ -10,7 +10,6 @@ namespace vision {
 
 using namespace ocarina;
 
-namespace hash_grid {
 static constexpr auto HASH_GRID_POSITION_BIT_NUM = 17;
 static constexpr auto HASH_GRID_POSITION_BIT_MASK = ((1u << HASH_GRID_POSITION_BIT_NUM) - 1);
 static constexpr auto HASH_GRID_LEVEL_BIT_NUM = 10;
@@ -26,10 +25,43 @@ static constexpr auto HASH_GRID_ALLOW_COMPACTION = (HASH_GRID_HASH_MAP_BUCKET_SI
 static constexpr auto HASH_GRID_POSITION_OFFSET = float3(0.0f, 0.0f, 0.0f);
 static constexpr auto HASH_GRID_POSITION_BIAS = 1e-4f;// may require adjustment for extreme scene scales;
 static constexpr auto HASH_GRID_NORMAL_BIAS = 1e-3f;
-}// namespace hash_grid
+static constexpr auto SHARC_SAMPLE_NUM_BIT_NUM = 18;
+static constexpr auto SHARC_SAMPLE_NUM_BIT_OFFSET = 0;
+static constexpr auto SHARC_SAMPLE_NUM_BIT_MASK = ((1u << SHARC_SAMPLE_NUM_BIT_NUM) - 1);
+static constexpr auto SHARC_ACCUMULATED_FRAME_NUM_BIT_NUM = 6;
+static constexpr auto SHARC_ACCUMULATED_FRAME_NUM_BIT_OFFSET = (SHARC_SAMPLE_NUM_BIT_NUM);
+static constexpr auto SHARC_ACCUMULATED_FRAME_NUM_BIT_MASK = ((1u << SHARC_ACCUMULATED_FRAME_NUM_BIT_NUM) - 1);
+static constexpr auto SHARC_STALE_FRAME_NUM_BIT_NUM = 8;
+static constexpr auto SHARC_STALE_FRAME_NUM_BIT_OFFSET = (SHARC_SAMPLE_NUM_BIT_NUM + SHARC_ACCUMULATED_FRAME_NUM_BIT_NUM);
+static constexpr auto SHARC_STALE_FRAME_NUM_BIT_MASK = ((1u << SHARC_STALE_FRAME_NUM_BIT_NUM) - 1);
+static constexpr auto SHARC_GRID_LOGARITHM_BASE = 2.0f;
+static constexpr auto SHARC_GRID_LEVEL_BIAS = 0;// positive bias adds extra levels with content magnification (can be negative as well);
+static constexpr auto SHARC_ENABLE_COMPACTION = HASH_GRID_ALLOW_COMPACTION;
+static constexpr auto SHARC_BLEND_ADJACENT_LEVELS = 1;// combine the data from adjacent levels on camera movement;
+static constexpr auto SHARC_DEFERRED_HASH_COMPACTION = (SHARC_ENABLE_COMPACTION && SHARC_BLEND_ADJACENT_LEVELS);
+static constexpr auto SHARC_NORMALIZED_SAMPLE_NUM = (1u << (SHARC_SAMPLE_NUM_BIT_NUM - 1));
+static constexpr auto SHARC_ACCUMULATED_FRAME_NUM_MIN = 1;                                   // minimum number of frames to use for data accumulation;
+static constexpr auto SHARC_ACCUMULATED_FRAME_NUM_MAX = SHARC_ACCUMULATED_FRAME_NUM_BIT_MASK;// maximum number of frames to use for data accumulation;
+                                                                                             // increase sample count internally to make resolve step with low sample count more robust, power of 2 usage may help compiler with optimizations;
+static constexpr auto SHARC_SAMPLE_NUM_MULTIPLIER = 16;
+static constexpr auto SHARC_SAMPLE_NUM_THRESHOLD = 0;// elements with sample count above this threshold will be used for early-out/resampling;
 
-namespace sharc {
+static constexpr auto SHARC_SEPARATE_EMISSIVE = 0;// if set, emissive values should be passed separately on updates and added to the cache query;
 
-}// namespace sharc
+static constexpr auto SHARC_INCLUDE_DIRECT_LIGHTING = 1;// if set cache values include both direct and indirect lighting;
+
+static constexpr auto SHARC_PROPOGATION_DEPTH = 4;// controls the amount of vertices stored in memory for signal backpropagation;
+
+static constexpr auto SHARC_UPDATE = 1;
+static constexpr auto SHARC_ENABLE_CACHE_RESAMPLING = (SHARC_UPDATE && (SHARC_PROPOGATION_DEPTH > 1));// resamples the cache during update step;
+
+static constexpr auto SHARC_RESAMPLING_DEPTH_MIN = 1;// controls minimum path depth which can be used with cache resampling;
+
+static constexpr auto SHARC_RADIANCE_SCALE = 1e3f;// scale used for radiance values accumulation. Each component uses 32-bit integer for data storage;
+
+static constexpr auto SHARC_STALE_FRAME_NUM_MIN = 8;// minimum number of frames to keep the element in the cache;
+
+static constexpr auto SHARC_DEBUG_BITS_OCCUPANCY_THRESHOLD_LOW = 0.125f;
+static constexpr auto SHARC_DEBUG_BITS_OCCUPANCY_THRESHOLD_MEDIUM = 0.5f;
 
 }// namespace vision
--- src/render_core/radiancecache/sharc/hash_grid.h
+++ src/render_core/radiancecache/sharc/hash_grid.h
@@ -7,9 +7,116 @@
 #include "core/stl.h"
 #include "dsl/dsl.h"
 #include "constant.h"
+#include "base/vs_header.h"
 
 namespace vision {
+using namespace ocarina;
+struct HashGridParameters {
+    float3 cameraPosition{};
+    float logarithmBase{};
+    float sceneScale{};
+    float levelBias{};
+};
+}// namespace vision
 
+OC_STRUCT(vision, HashGridParameters, cameraPosition,
+          logarithmBase, sceneScale, levelBias){};
 
-
+namespace vision {
+using namespace ocarina;
+template<EPort p = H>
+oc_float<p> HashGridLogBase_impl(const oc_float<p> &x, const oc_float<p> &base) {
+    return ocarina::log(x) / ocarina::log(base);
 }
+VS_MAKE_CALLABLE(HashGridLogBase)
+
+//template<EPort p = D>
+uint HashGridGetBaseSlot(uint slot, uint capacity) {
+    return (slot / HASH_GRID_HASH_MAP_BUCKET_SIZE) * HASH_GRID_HASH_MAP_BUCKET_SIZE;
+}
+
+uint HashGridHashJenkins32(uint a) {
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    return a;
+}
+
+uint HashGridHash32(uint64_t hashKey) {
+    return HashGridHashJenkins32(uint((hashKey >> 0) & 0xFFFFFFFF)) ^ HashGridHashJenkins32(uint((hashKey >> 32) & 0xFFFFFFFF));
+}
+
+auto HashGridGetLevel(float3 samplePosition, HashGridParameters gridParameters) {
+    const float distance2 = dot(gridParameters.cameraPosition - samplePosition, gridParameters.cameraPosition - samplePosition);
+
+    return uint(ocarina::clamp(0.5f * HashGridLogBase<H>(distance2, gridParameters.logarithmBase) + gridParameters.levelBias, 1.0f, float(HASH_GRID_LEVEL_BIT_MASK)));
+}
+
+float HashGridGetVoxelSize(uint gridLevel, HashGridParameters gridParameters) {
+    return ocarina::pow(gridParameters.logarithmBase, float(gridLevel)) / (gridParameters.sceneScale * pow(gridParameters.logarithmBase, gridParameters.levelBias));
+}
+
+int4 HashGridCalculatePositionLog(float3 samplePosition, HashGridParameters gridParameters) {
+    samplePosition += float3(HASH_GRID_POSITION_BIAS, HASH_GRID_POSITION_BIAS, HASH_GRID_POSITION_BIAS);
+
+    uint gridLevel = HashGridGetLevel(samplePosition, gridParameters);
+    float voxelSize = HashGridGetVoxelSize(gridLevel, gridParameters);
+    int3 gridPosition = int3(floor(samplePosition / voxelSize));
+
+    return make_int4(gridPosition.xyz(), gridLevel);
+}
+
+uint64_t HashGridComputeSpatialHash(float3 samplePosition, float3 sampleNormal, HashGridParameters gridParameters) {
+    uint4 gridPosition = uint4(HashGridCalculatePositionLog(samplePosition, gridParameters));
+
+    uint64_t hashKey = ((uint64_t(gridPosition.x) & HASH_GRID_POSITION_BIT_MASK) << (HASH_GRID_POSITION_BIT_NUM * 0)) |
+                       ((uint64_t(gridPosition.y) & HASH_GRID_POSITION_BIT_MASK) << (HASH_GRID_POSITION_BIT_NUM * 1)) |
+                       ((uint64_t(gridPosition.z) & HASH_GRID_POSITION_BIT_MASK) << (HASH_GRID_POSITION_BIT_NUM * 2)) |
+                       ((uint64_t(gridPosition.w) & HASH_GRID_LEVEL_BIT_MASK) << (HASH_GRID_POSITION_BIT_NUM * 3));
+
+    uint normalBits =
+        (sampleNormal.x + HASH_GRID_NORMAL_BIAS >= 0 ? 0 : 1) +
+        (sampleNormal.y + HASH_GRID_NORMAL_BIAS >= 0 ? 0 : 2) +
+        (sampleNormal.z + HASH_GRID_NORMAL_BIAS >= 0 ? 0 : 4);
+
+    hashKey |= (uint64_t(normalBits) << (HASH_GRID_POSITION_BIT_NUM * 3 + HASH_GRID_LEVEL_BIT_NUM));
+
+    return hashKey;
+}
+
+float3 HashGridGetPositionFromKey(const uint64_t hashKey, HashGridParameters gridParameters) {
+    const int signBit = 1 << (HASH_GRID_POSITION_BIT_NUM - 1);
+    const int signMask = ~((1 << HASH_GRID_POSITION_BIT_NUM) - 1);
+
+    int3 gridPosition;
+    gridPosition.x = int((hashKey >> (HASH_GRID_POSITION_BIT_NUM * 0)) & HASH_GRID_POSITION_BIT_MASK);
+    gridPosition.y = int((hashKey >> (HASH_GRID_POSITION_BIT_NUM * 1)) & HASH_GRID_POSITION_BIT_MASK);
+    gridPosition.z = int((hashKey >> (HASH_GRID_POSITION_BIT_NUM * 2)) & HASH_GRID_POSITION_BIT_MASK);
+
+    // Fix negative coordinates
+    gridPosition.x = (gridPosition.x & signBit) != 0 ? gridPosition.x | signMask : gridPosition.x;
+    gridPosition.y = (gridPosition.y & signBit) != 0 ? gridPosition.y | signMask : gridPosition.y;
+    gridPosition.z = (gridPosition.z & signBit) != 0 ? gridPosition.z | signMask : gridPosition.z;
+
+    uint gridLevel = uint((hashKey >> HASH_GRID_POSITION_BIT_NUM * 3) & HASH_GRID_LEVEL_BIT_MASK);
+    float voxelSize = HashGridGetVoxelSize(gridLevel, gridParameters);
+    float3 samplePosition = (gridPosition + 0.5f) * voxelSize;
+
+    return samplePosition;
+}
+
+}// namespace vision
+
+namespace vision {
+struct HashMapData {
+    uint capacity{};
+    BufferDesc<uint64_t> hashEntriesBuffer;
+    BufferDesc<uint> lockBuffer;
+};
+}// namespace vision
+
+OC_PARAM_STRUCT(vision, HashMapData, capacity,
+                hashEntriesBuffer, lockBuffer){};
