c2696522e47bebfea46d6629dfdd44013a2dbef7
Zero <249002031@qq.com>
2025-07-25 11:35:25 +0800

    update sharc

--- src/render_core/radiancecache/sharc/sharc.h
+++ src/render_core/radiancecache/sharc/sharc.h
@@ -15,17 +15,49 @@ struct SharcParameters {
     BufferDesc<uint4> voxelDataBufferPrev;
 };
 }// namespace vision
-
-OC_PARAM_STRUCT(vision, SharcParameters,gridParameters, hashMapData, enableAntiFireflyFilter,
+OC_PARAM_STRUCT(vision, SharcParameters, gridParameters, hashMapData, enableAntiFireflyFilter,
                 voxelDataBuffer, voxelDataBufferPrev){};
 
 namespace vision {
 struct SharcState {
     std::array<uint, SHARC_PROPOGATION_DEPTH> cacheIndices{};
-    std::array<float, 2> sampleWeights;
+    std::array<float3, SHARC_PROPOGATION_DEPTH> sampleWeights;
     uint pathLength{};
 };
 }// namespace vision
-
 OC_STRUCT(vision, SharcState, cacheIndices,
           sampleWeights, pathLength){};
+
+namespace vision {
+struct SharcHitData {
+    float3 positionWorld;
+    float3 normalWorld;
+    float3 emissive;
+};
+}// namespace vision
+OC_STRUCT(vision, SharcHitData, positionWorld,
+          normalWorld, emissive){};
+
+namespace vision {
+struct SharcVoxelData {
+    uint3 accumulatedRadiance;
+    uint accumulatedSampleNum;
+    uint accumulatedFrameNum;
+    uint staleFrameNum;
+};
+}// namespace vision
+OC_STRUCT(vision, SharcVoxelData, accumulatedRadiance,
+          accumulatedSampleNum, accumulatedFrameNum,
+          staleFrameNum){};
+
+namespace vision {
+struct SharcResolveParameters {
+    float3 cameraPositionPrev;
+    uint accumulationFrameNum;
+    uint staleFrameNumMax;
+    uint enableAntiFireflyFilter;
+};
+}// namespace vision
+OC_STRUCT(vision, SharcResolveParameters, cameraPositionPrev,
+          accumulationFrameNum, staleFrameNumMax,
+          enableAntiFireflyFilter){};
